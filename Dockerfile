fbe6ed3d9ff3df952f83fa4063a008c7201e97cf
Alfredo Matas <amatas@gmail.com>
2015-02-04 12:33:14 +0100

    Removed killall java command

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,5 @@
 FROM inclusivedesign/java:openjdk-7
 
-
-
 RUN rpm --import http://caucho.com/download/rpm/RPM-GPG-KEY-caucho && \
     yum install -y http://caucho.com/download/rpm-6.5/4.0.42/x86_64/resin-4.0.42-1.x86_64.rpm && \
     yum -y install java-1.7.0-openjdk-devel psmisc
--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 
 cat >/etc/supervisord.d/resin.ini<<EOF
 [program:resin]
-command="killall java;/usr/bin/java -d64 -jar /usr/local/share/resin-4.0.42/lib/resin.jar -root-directory /var/resin -conf /etc/resin/resin.xml start-with-foreground"
+command=/usr/bin/java -d64 -jar /usr/local/share/resin-4.0.42/lib/resin.jar -root-directory /var/resin -conf /etc/resin/resin.xml start-with-foreground
 user=resin
 autorestart=true
 redirect_stderr=true
